80d4c3a5a1b62475139211f874ba46b8605a7bd9
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-09 10:28:44 +0530

    fix: add singleton instance of PrismaClient

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import { prisma } from "@/lib/prisma";
 export async function createUser(
   username: string,
   email: string,
