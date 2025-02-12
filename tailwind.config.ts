bc4349647de9a44730e3e26f9a0ba0ce7fa592f8
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-12 12:45:09 +0530

    style: add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blue: "#00FFFF",
+        green: "#39FF14",
       },
     },
   },
