a3ec0f96873ef7f93b94380e5515163487cf3fa7
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-15 12:04:10 +0530

    db: add role to users and add migrations

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
+import { prisma } from "@/lib/prisma";
+import { Role } from "@prisma/client";
 export async function createUser(
   username: string,
   email: string,
-  password: string
+  password: string,
+  role: Role = "USER"
 ) {
   return prisma.user.create({
     data: {
       username,
       email,
       password,
+      role,
     },
   });
 }
