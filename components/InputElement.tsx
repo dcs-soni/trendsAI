ca3f2fff557dd62c59a93de9fb7647a7ab35ba05
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-18 23:13:11 +0530

    fix: add types and props for input element

--- components/InputElement.tsx
+++ components/InputElement.tsx
@@ -1,19 +1,29 @@
+import { twMerge } from "tailwind-merge";
+
 const InputElement = ({
   htmlFor,
   children,
   reference,
   placeholder,
   type,
+  value,
+  onChange,
+  className,
+  labelClassname,
 }: {
   htmlFor: string;
   children: React.ReactNode;
-  reference: React.RefObject<HTMLInputElement>;
+  reference?: React.RefObject<HTMLInputElement>;
   placeholder: string;
   type: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  labelClassname?: string;
 }) => {
   return (
     <div>
-      <label htmlFor={htmlFor} className="sr-only">
+      <label htmlFor={htmlFor} className={twMerge("", labelClassname)}>
         {children}
       </label>
 
@@ -23,8 +33,13 @@ const InputElement = ({
         name="username"
         type={type}
         required
-        className="appearance-none relative block w-full px-4 py-4 bg-white/5 border border-white/10 placeholder-gray-400 text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors"
+        className={twMerge(
+          "appearance-none relative block w-full px-4 py-4 bg-white/5 border border-white/10 placeholder-gray-400 text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors",
+          className
+        )}
         placeholder={placeholder}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
